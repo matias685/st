c34dc2bd815d41bf02d7edadad912396acc22d23
matias685 <78141961+matias685@users.noreply.github.com>
2021-02-03 01:39:53 +0200

    Add files via upload

--- config.def.h
+++ config.def.h
@@ -5,10 +5,10 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Hack Nerd Font Mono:pixelsize=18:antialias=true:autohint=true";
+static char *font = "Hack:style=Regular:pixelsize=18:antialias=true:autohint=true";
 /* Spare fonts */
 static char *font2[] = {
-	"Inconsolata for Powerline:pixelsize=18:antialias=true:autohint=true", 
+/*	"Inconsolata for Powerline:pixelsize=18:antialias=true:autohint=true", */ 
 /*	"Hack Nerd Font Mono:pixelsize=18:antialias=true:autohint=true", */
 };
 
@@ -99,6 +99,9 @@ char *termname = "st-256color";
  */
 unsigned int tabspaces = 8;
 
+/* bg opacity */
+float alpha = 0.8;
+
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
 	/* 8 normal colors */
@@ -126,6 +129,7 @@ static const char *colorname[] = {
 	/* more colors can be added after 255 to use with DefaultXX */
 	"#191919",
 	"#d8dee9",
+	"#181818",
 };
 
 
@@ -134,7 +138,7 @@ static const char *colorname[] = {
  * foreground, background, cursor, reverse cursor
  */
 unsigned int defaultfg = 257;
-unsigned int defaultbg = 256;
+unsigned int defaultbg = 258;
 static unsigned int defaultcs = 257;
 static unsigned int defaultrcs = 256;
 
