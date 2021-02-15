359532c7fa5afa3bc7f1bfd5b5db5ce4f79385dc
matias685 <78141961+matias685@users.noreply.github.com>
2021-02-15 22:43:17 +0200

    Add files via upload

--- config.h
+++ config.h
@@ -8,7 +8,7 @@
 static char *font = "Hack:style=Regular:pixelsize=18:antialias=true:autohint=true";
 /* Spare fonts */
 static char *font2[] = {
-/*	"Inconsolata for Powerline:pixelsize=12:antialias=true:autohint=true", */
+	"FontAwesome:style=Regular:pixelsize=18:antialias=true:autohint=true", 
 /*	"Hack Nerd Font Mono:pixelsize=11:antialias=true:autohint=true", */
 };
 
